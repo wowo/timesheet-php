9d023b31712f055730158461f620c9e0f4439de5
Wojciech Sznapka <wojciech@sznapka.pl>
2010-03-01 23:42:13 +0100

    Added symfony libs into project folder - freeze

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once '/usr/share/php/symfony/autoload/sfCoreAutoload.class.php';
+//require_once '/usr/share/php/symfony/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__) . '/../lib/symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
