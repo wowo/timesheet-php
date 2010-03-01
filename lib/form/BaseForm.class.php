b7c9eabc132beff21a2eca2b9e8564a4e72e04b5
Wojciech Sznapka <wojciech@sznapka.pl>
2010-03-01 19:38:21 +0100

    Initial import

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    timesheet
+ * @subpackage form
+ * @author     Wojciech Sznapka 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
